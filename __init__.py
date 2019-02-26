84ebab89ba4c362de2dc34b49b6e776270a4957e
CzTay3103 <34021885+CzJakethedog@users.noreply.github.com>
2019-02-26 10:00:55 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .imagetoarraypreprocessor import ImageToArrayPreprocessor
+from .simplepreprocessor import SimplePreprocessor
+from .aspectawarepreprocessor import AspectAwarePreprocessor
+from .meanpreprocessor import MeanPreprocessor
+from .patchpreprocessor import PatchPreprocessor
+from .croppreprocessor import CropPreprocessor
---
+++ croppreprocessor.py
@@ -0,0 +1,36 @@
+# import lib
+import numpy as np
+import cv2
+
+class CropPreprocessor:
+	def __init__(self, width, height, horiz=True, inter=cv2.INTER_AREA):
+		self.width = width
+		self.height = height
+		self.horiz = horiz
+		self.inter = inter
+	
+	def preprocess(self, image):
+		# initialize the list of crops
+		crops = []
+		
+		(h, w) = image.shape[:2]
+		# top-left, top-right, bottom-right, bottom-left, respectively
+		coords = [[0, 0, self.width, self.height], [w - self.width, 0, w, self.height], [w - self.width, h - self.height, w, h], [0, h - self.height, self.width, h]]
+		
+		# compute the center crop of the image as well
+		dW = int(0.5 * (w - self.width))
+		dH = int(0.5 * (h -self.height))
+		coords.append([dW, dH, w - dW, h - dH])
+		
+		# loop over the coords, extract each of the crops + resize everyone
+		for (startX, startY, endX, endY) in coords:
+			crop = image[startY:endY, startX:endX]
+			crop = cv2.resize(crop, (self.width, self.height), interpolation=self.inter)
+			crops.append(crop)
+
+		if self.horiz:
+			# the horizontal mirror flips for each crop
+			mirrors = [cv2.flip(c, 1) for c in crops]
+			crops.extend(mirrors)
+		
+		return np.array(crops)
